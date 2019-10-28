d4fc41145f033e97a9bb24ede1d9f4c96bd8a4a0
dclife <dclife1990@gmail.com>
2019-10-28 16:53:58 +0200

    change naming

--- dclife/AppDelegate.swift
+++ dclife/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    let flow = Main.UI.Flow.Default()
+    let flow = Main.UI.Flow.Initial()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window?.rootViewController = flow.initialViewController
