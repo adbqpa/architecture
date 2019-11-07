6b3e4060ef29138aef0f8b98fdc1d95f62b3e350
dclife <dclife1990@gmail.com>
2019-11-07 06:18:19 +0200

    add fixes to architecture

--- DCModuleUI/Flow/Flow.swift
+++ DCModuleUI/Flow/Flow.swift
@@ -6,24 +6,24 @@ import UIKit
 import DCModule
 
 public protocol ModuleUIFlowObjectProtocol: Module.UI.ComponentProtocol {
-    var parent: Module.UI.Flow.ObjectProtocol? { get set }
-    var initialViewController: UIViewController? { get }
+    var parent: Module.UI.Flow.ObjectProtocol! { get set }
+    var initialViewController: UIViewController! { get }
 }
 
 public protocol ModuleUIFlowNavigationProtocol: Module.UI.Flow.ObjectProtocol {
-    var navigationController: UINavigationController? { get }
+    var navigationController: UINavigationController! { get }
 }
 
 public extension Module.UI.Flow.NavigationProtocol {
-    var initialViewController: UIViewController? { return navigationController }
+    var initialViewController: UIViewController! { return navigationController }
 }
 
 public protocol ModuleUIFlowTabProtocol: Module.UI.Flow.ObjectProtocol {
-    var tabBarController: UITabBarController? { get }
+    var tabBarController: UITabBarController! { get }
 }
 
 public extension Module.UI.Flow.TabProtocol {
-    var initialViewController: UIViewController? { return tabBarController }
+    var initialViewController: UIViewController! { return tabBarController }
 }
 
 extension Module.UI.Flow {
