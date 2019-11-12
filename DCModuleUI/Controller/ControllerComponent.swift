2dcc7ce6e4e961355f1192497d072331d38faf54
dclife <dclife1990@gmail.com>
2019-11-12 08:29:24 +0200

    update architecture

--- DCModuleUI/Controller/ControllerComponent.swift
+++ DCModuleUI/Controller/ControllerComponent.swift
@@ -7,11 +7,11 @@ import DCModule
 
 open class ModuleUIControllerComponent: UIViewController, Module.UI.ComponentProtocol {
     public weak var flow: Module.UI.Flow.Component!
-    public class var bundle: Bundle { return Bundle(for: self) }
-    public class var storyboardName: String { return bundle.bundleURL.lastPathComponent.replacingOccurrences(of: ".framework", with: "") }
-    public class var storyboard: UIStoryboard { return UIStoryboard(name: storyboardName, bundle: bundle) }
-    public class var storyboardIdentifier: String { return "default" }
-    public class func viewController() -> Self {
+    open class var bundle: Bundle { return Bundle(for: self) }
+    open class var storyboardName: String { return bundle.bundleURL.lastPathComponent.replacingOccurrences(of: ".framework", with: "") }
+    open class var storyboard: UIStoryboard { return UIStoryboard(name: storyboardName, bundle: bundle) }
+    open class var storyboardIdentifier: String { return "default" }
+    open class func viewController() -> Self {
         return storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
     }
 }
